a18f541771e5301e657039726a9ce99f394752c3
Vignesh <vigneshuvi@gmail.com>
2018-02-01 20:33:15 +0530

    Updated the version with Android fix.

--- src/android/NativeAlert.java
+++ src/android/NativeAlert.java
@@ -4,68 +4,72 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Context;
 
+
+/**
+ * This class NativeAlert a string called from JavaScript.
+ */
 public class NativeAlert extends CordovaPlugin {
+    private final Context context;
+
+    public NativeAlert(Context context) {
+        this.context = context;
+    }
 
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        if (action.equals("showAlert")) { //
-
-           cordova.getThreadPool().execute(new Runnable() {
-            
-                public void run() {  
-                    // Thread-safe.
-                    if (leaguecountArray.length()  > 0) {
-                        String jsonString = data.getString(0);
-                        JSONObject jsonObj = new JSONObject(jsonString);
-                        String title = (json.getString("title") != null) ? json.getString("title") : "Alert";
-                        String message = (json.getString("message") != null) ? json.getString("message") : "";
-                        String ok = (json.getString("okButton") != null) ? json.getString("okButton") : "Ok";
-                        String cancel = json.getString("cancelButton");
-                        this.show(title, message, ok, cancel, callbackContext);
-                    } else {
-                        this.callback(null, callbackContext);
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("showAlert")) {
+            if (args.length() > 0) {
+                String jsonString = args.getString(0);
+                JSONObject json = new JSONObject(jsonString);
+                String title = (json.getString("title") != null) ? json.getString("title") : "Alert";
+                String message = (json.getString("message") != null) ? json.getString("message") : "";
+                String ok = (json.getString("okButton") != null) ? json.getString("okButton") : "Ok";
+                String cancel = json.getString("cancelButton");
+                cordova.getThreadPool().execute(new Runnable() {
+                    public void run() { // Thread-safe.
+                        show(title, message, ok, cancel, callbackContext);
                     }
-                }
-
-            });
-            return true;
-        } else {
-            return false;
-
+                });
+                return true;
+            }
         }
+        sendPluginResult(null, callbackContext);
+        return false;
     }
 
     private void show(String title, String message, String ok, String cancel, CallbackContext callbackContext) {
-        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
         alertDialog.setTitle(title);
         alertDialog.setMessage(message);
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, ok,
-            new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                     this.callback(ok, callbackContext);
-                    dialog.dismiss();
-                }
-            });
-        if(cancel != null) {
-            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, cancel,
+        alertDialog.setPositiveButton(ok,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                         this.callback(cancel, callbackContext);
+                        sendPluginResult(ok, callbackContext);
                         dialog.dismiss();
                     }
-            });
+                });
+        if(cancel != null) {
+            alertDialog.setNegativeButton(cancel,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            sendPluginResult(cancel, callbackContext);
+                            dialog.dismiss();
+                        }
+                    });
         }
         alertDialog.show();
     }
 
-    // Validate the message and send callback accordingly.
-    private void callback(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) { 
+
+    private void sendPluginResult(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
-            callbackContext.error("Echo Argument was null");
+            callbackContext.error("Alert Argument was null");
         }
     }
 }
