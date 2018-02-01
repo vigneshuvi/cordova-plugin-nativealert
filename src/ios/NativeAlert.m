006cbda64c11b9c8d5b57da616927d8fc36f4c94
Vignesh <vigneshuvi@gmail.com>
2018-02-01 19:30:07 +0530

    Refactor iOS code.

--- src/ios/NativeAlert.m
+++ src/ios/NativeAlert.m
@@ -34,38 +34,11 @@
             NSData *data = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
             id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err];
             if (err == nil && json != nil) {
-                NSString *title = [json objectForKey:@"title"];
-                NSString *message = [json objectForKey:@"message"];
-                NSString *ok = [json objectForKey:@"okButton"];
-                UIAlertController * alert=[UIAlertController alertControllerWithTitle:(title != nil ? title : @"Alert")
-                                                                              message:(message != nil ? message : @"")
-                                                                       preferredStyle:UIAlertControllerStyleAlert];
-                
-                UIAlertAction* yesButton = [UIAlertAction actionWithTitle: (ok != nil ? ok : @"Ok")
-                                                                    style:UIAlertActionStyleDefault
-                                                                  handler:^(UIAlertAction * action) {
-                                                result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:(ok != nil ? ok : @"Ok")];
-                                                [self.commandDelegate sendPluginResult:result callbackId:callbackId];
-                                            }];
-                [alert addAction:yesButton];
+                NSString *title = [json objectForKey:@"title"] != nil ? [json objectForKey:@"title"] : @"Alert";
+                NSString *message = [json objectForKey:@"message"] != nil ? [json objectForKey:@"message"] : @"";
+                NSString *ok = [json objectForKey:@"okButton"] != nil ? [json objectForKey:@"okButton"] : @"Ok";
                 NSString *cancel = [json objectForKey:@"cancelButton"];
-                if (cancel != nil) {
-                    UIAlertAction* yesButton = [UIAlertAction actionWithTitle: (cancel != nil ? cancel : @"Cancel")
-                                                                    style:UIAlertActionStyleDefault
-                                                                  handler:^(UIAlertAction * action) {
-                                                result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:(cancel != nil ? cancel : @"Cancel")];
-                                                [self.commandDelegate sendPluginResult:result callbackId:callbackId];
-                                                
-                                            }];
-                    [alert addAction:yesButton];
-                }
-                if (IS_IPAD) {
-                    CGRect rect = CGRectMake( (kWindowWidth - 300) / 2,  100, 300, 400);
-                    alert.popoverPresentationController.sourceView = alert.view;
-                    alert.popoverPresentationController.sourceRect = rect;
-                    alert.popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirectionUp;
-                }
-                [[self currentApplicationViewController] presentViewController:alert animated:YES completion:nil];
+                [self show:title withMessage:message withOk:ok withCancel:cancel withCallbackId:callbackId];
             } else {
                 result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Alert Argument was null"];
                 [self.commandDelegate sendPluginResult:result callbackId:callbackId];
@@ -75,6 +48,39 @@
             [self.commandDelegate sendPluginResult:result callbackId:callbackId];
         }
     }];
+}
+
+-(void) show:(NSString*)title withMessage:(NSString*)message withOk:(NSString*)ok withCancel:(NSString*)cancel withCallbackId:(NSString*)callbackId {
+    __block CDVPluginResult* result = nil;
+    UIAlertController * alert=[UIAlertController alertControllerWithTitle:title
+                                                                  message:message
+                                                           preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* yesButton = [UIAlertAction actionWithTitle:ok
+                                                        style:UIAlertActionStyleDefault
+                                                      handler:^(UIAlertAction * action) {
+                                                          result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:(ok != nil ? ok : @"Ok")];
+                                                          [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+                                                      }];
+    [alert addAction:yesButton];
+   
+    if (cancel != nil) {
+        UIAlertAction* yesButton = [UIAlertAction actionWithTitle: (cancel != nil ? cancel : @"Cancel")
+                                                            style:UIAlertActionStyleDefault
+                                                          handler:^(UIAlertAction * action) {
+                                                              result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:(cancel != nil ? cancel : @"Cancel")];
+                                                              [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+                                                              
+                                                          }];
+        [alert addAction:yesButton];
+    }
+    if (IS_IPAD) {
+        CGRect rect = CGRectMake( (kWindowWidth - 300) / 2,  100, 300, 400);
+        alert.popoverPresentationController.sourceView = alert.view;
+        alert.popoverPresentationController.sourceRect = rect;
+        alert.popoverPresentationController.permittedArrowDirections = UIPopoverArrowDirectionUp;
+    }
+    [[self currentApplicationViewController] presentViewController:alert animated:YES completion:nil];
 }
 
 /**
